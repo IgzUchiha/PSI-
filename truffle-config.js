ea69f73e76902d6cd07f3b0e2d6e5d4ba0220e2d
junior <junior@Igmers-MacBook-Pro.local>
2021-12-17 21:18:33 -0800

    First commit

---
+++ truffle-config.js
@@ -0,0 +1,47 @@
+require('babel-register');
+require('babel-polyfill');
+require('dotenv').config();
+
+module.exports = {
+  networks: {
+    developement: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+  },
+   contracts_directory: './src/contracts/',
+   contracts_build_directory: './src/abis/',
+   compilers: {
+     solc: {
+       optimizer: {
+         enabled: true,
+         runs: 200
+       }
+     }
+   },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    },
+  },
+};
